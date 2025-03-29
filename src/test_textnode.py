c7b155272b1a4bf0d86d7c451135fbaf6c40b1f8
elhostakosta <196651231+elhostakosta@users.noreply.github.com>
2025-03-29 07:44:00 +0200

    Rewrite the test cases for TextNode class

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,43 +1,37 @@
 import unittest
 
-from textnode import (
-    TextNode,
-    text_type_text,
-    text_type_bold,
-    text_type_italic,
-    text_type_code,
-    text_type_image,
-    text_type_link,
-)
+from textnode import TextNode, TextType
+
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node = TextNode("This is a text node", text_type_bold)
-        node2 = TextNode("This is a text node", text_type_bold)
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
     def test_text(self):
-        node = TextNode("This is a text node", text_type_bold)
-        self.assertEqual(node.text, "This is a text node")
-
+        node = TextNode("LOOK AT ME!", TextType.ITALIC)
+        self.assertEqual(node.text, "LOOK AT ME!")
+        
     def test_url(self):
-        node = TextNode("I am a text node", text_type_bold)
-        node2 = TextNode("I am also a text node", text_type_bold, "https://example.com/")
-        self.assertEqual(node.url, None)
-        self.assertEqual(node2.url, "https://example.com/")
-
-    def test_text_type(self):
-        node = TextNode("A text node", text_type_bold)
-        self.assertEqual(node.text_type, text_type_bold)
+        node = TextNode("Not you again!", TextType.LINK, "https://example.com/")
+        node2 = TextNode("Who am I?", TextType.LINK)
+        self.assertEqual(None, node2.url)
+        self.assertEqual(node.url, "https://example.com/")
 
     def test_non_eq(self):
-        node1 = TextNode("First text node", text_type_italic)
-        node2 = TextNode("Second text node", text_type_bold)
+        node1 = TextNode("We have the same text!", TextType.LINK)
+        node2 = TextNode("We have the same text!", TextType.ITALIC)
+        node3 = TextNode("Tommy", TextType.LINK)
+        node4 = TextNode("Trevor", TextType.LINK)
         self.assertNotEqual(node1, node2)
+        self.assertNotEqual(node3, node4)
 
     def test_repr(self):
-        node = TextNode("I'm a text node", text_type_italic, "https://chatgpt.com/")
-        self.assertEqual(repr(node), "TextNode(I'm a text node, italic, https://chatgpt.com/)")
+        node = TextNode("I'm a text node", TextType.ITALIC, "https://example.com/")
+        node2 = TextNode("I'm a text node", TextType.ITALIC)
+        self.assertEqual(repr(node), "TextNode(I'm a text node, italic, https://example.com/)")
+        self.assertEqual(repr(node2), "TextNode(I'm a text node, italic, None)")
 
 if __name__ == "__main__":
     unittest.main()
