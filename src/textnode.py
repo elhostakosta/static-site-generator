5d4350dcfc686199723a690082edf28a7d1f4247
elhostakosta <196651231+elhostakosta@users.noreply.github.com>
2025-03-29 06:46:58 +0200

    Create TextType enum

--- src/textnode.py
+++ src/textnode.py
@@ -1,9 +1,12 @@
-text_type_text = "text"
-text_type_bold = "bold"
-text_type_italic = "italic"
-text_type_code = "code"
-text_type_link = "link"
-text_type_image = "image"
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
 
 class TextNode:
     def __init__(self, text, text_type, url=None):
@@ -19,4 +22,4 @@ class TextNode:
         )
 
     def __repr__(self):
-        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
