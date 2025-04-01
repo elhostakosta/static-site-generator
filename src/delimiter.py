948043443ecf3d942e5ec87b3b3c9bbda7d278d4
elhostakosta <196651231+elhostakosta@users.noreply.github.com>
2025-04-01 22:30:01 +0200

    Check for empty strings before processing them

--- src/delimiter.py
+++ src/delimiter.py
@@ -8,18 +8,17 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         if node.text_type != TextType.TEXT:
             new_nodes.append(node)
         else:
-            if delimiter in node.text:
-                splitted_list = node.text.split(delimiter)
-                if (len(splitted_list) % 2 == 0):
-                    raise ValueError("The matching closing delimiter is not found!")
-                else:
-                    processed_nodes = []
-                    for i in range(0, len(splitted_list)):
-                        if (i % 2 == 0):
-                            processed_nodes.append(TextNode(splitted_list[i], TextType.TEXT))
-                        else:
-                            processed_nodes.append(TextNode(splitted_list[i], text_type))
-                    new_nodes.extend(processed_nodes)
+            splitted_list = node.text.split(delimiter)
+            if (len(splitted_list) % 2 == 0):
+                raise ValueError("The matching closing delimiter is not found!")
             else:
-                new_nodes.append(node)
+                processed_nodes = []
+                for i in range(0, len(splitted_list)):
+                    if (splitted_list[i] == ""):
+                        continue
+                    elif (i % 2 == 0):
+                        processed_nodes.append(TextNode(splitted_list[i], TextType.TEXT))
+                    else:
+                        processed_nodes.append(TextNode(splitted_list[i], text_type))
+                new_nodes.extend(processed_nodes)
     return new_nodes
